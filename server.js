9b42e9279027668f11c90b879690594bcb3faba6
Paweł Pyc <pawelpyc154@gmail.com>
2019-11-30 19:10:48 +0100

    v1

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express')
+const connectDB = require('./config/db')
 
 const app = express();
+
+//Connect Database
+connectDB()
+
+
+//Init Middleware
+app.use(express.json({
+    extended: false
+}))
 
 app.get('/', (req, res) => res.json({
     msg: 'siemanko Api'
