fd8153f81997692800c559c86c4682920b3f54b3
Paweł Pyc <pawelpyc154@gmail.com>
2019-12-02 12:16:57 +0100

    make it better

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-const express = require('express')
-const connectDB = require('./config/db')
+const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
 //Connect Database
-connectDB()
-
+connectDB();
 
 //Init Middleware
-app.use(express.json({
+app.use(
+  express.json({
     extended: false
-}))
+  })
+);
 
-app.get('/', (req, res) => res.json({
-    msg: 'siemanko Api'
-}));
+// app.get('/', (req, res) => res.json({
+//     msg: 'siemanko Api'
+// }));
 // Define Routes
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
-
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log("elo"))
+app.listen(PORT, () => console.log("elo"));
